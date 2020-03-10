57f6d6ddd0662f2bc32e85d5b10d01f753c46b5a
Jase M. Wilson <jase.wilson@aggiemail.usu.edu>
2020-03-10 10:11:17 -0600

    Cleaned up logic code

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -41,79 +41,7 @@ public class ConnectFour extends Application {
 				}
 
 				//LOGIC
-				//VERTICAL
-				for(int vi=0; vi<6; vi++){
-					VBox vv = (VBox) pane.getChildren().get(vi);
-					for(int vj=5; vj>2; vj--){
-						Circle c1 = (Circle) vv.getChildren().get(vj);
-						Circle c2 = (Circle) vv.getChildren().get(vj-1);
-						Circle c3 = (Circle) vv.getChildren().get(vj-2);
-						Circle c4 = (Circle) vv.getChildren().get(vj-3);
-						if((c1.getFill() == Color.YELLOW && c2.getFill() == Color.YELLOW && c3.getFill() == Color.YELLOW && c4.getFill() == Color.YELLOW) || (c1.getFill() == Color.RED && c2.getFill() == Color.RED && c3.getFill() == Color.RED && c4.getFill() == Color.RED)){
-							//FLASHING WIN
-							flashDisks(c1, c2, c3, c4, color);
-							//Disable clicking
-							disableClick(pane);
-						}
-					}
-				}
-				//HORIZONTAL
-				for(int hi=0; hi<4; hi++){
-					VBox hv1 = (VBox) pane.getChildren().get(hi);
-					VBox hv2 = (VBox) pane.getChildren().get(hi+1);
-					VBox hv3 = (VBox) pane.getChildren().get(hi+2);
-					VBox hv4 = (VBox) pane.getChildren().get(hi+3);
-					for(int hj=5; hj>=0; hj--){
-						Circle hc1 = (Circle) hv1.getChildren().get(hj);
-						Circle hc2 = (Circle) hv2.getChildren().get(hj);
-						Circle hc3 = (Circle) hv3.getChildren().get(hj);
-						Circle hc4 = (Circle) hv4.getChildren().get(hj);
-						if((hc1.getFill() == Color.YELLOW && hc2.getFill() == Color.YELLOW && hc3.getFill() == Color.YELLOW && hc4.getFill() == Color.YELLOW) || (hc1.getFill() == Color.RED && hc2.getFill() == Color.RED && hc3.getFill() == Color.RED && hc4.getFill() == Color.RED)){
-							//FLASHING WIN
-							flashDisks(hc1, hc2, hc3, hc4, color);
-							//Disable clicking
-							disableClick(pane);
-						}
-					}
-				}
-				//DIAGONAL: LEFT TO RIGHT
-				for(int di=0; di<4; di++){
-					VBox dv1 = (VBox) pane.getChildren().get(di);
-					VBox dv2 = (VBox) pane.getChildren().get(di+1);
-					VBox dv3 = (VBox) pane.getChildren().get(di+2);
-					VBox dv4 = (VBox) pane.getChildren().get(di+3);
-					for(int dj=5; dj>2; dj--){
-						Circle dc1 = (Circle) dv1.getChildren().get(dj);
-						Circle dc2 = (Circle) dv2.getChildren().get(dj-1);
-						Circle dc3 = (Circle) dv3.getChildren().get(dj-2);
-						Circle dc4 = (Circle) dv4.getChildren().get(dj-3);
-						if((dc1.getFill() == Color.YELLOW && dc2.getFill() == Color.YELLOW && dc3.getFill() == Color.YELLOW && dc4.getFill() == Color.YELLOW) || (dc1.getFill() == Color.RED && dc2.getFill() == Color.RED && dc3.getFill() == Color.RED && dc4.getFill() == Color.RED)){
-							//FLASHING WIN
-							flashDisks(dc1, dc2, dc3, dc4, color);
-							//Disable clicking
-							disableClick(pane);
-						}
-					}
-				}
-				//DIAGONAL: RIGHT TO LEFT
-				for(int dri=6; dri>2; dri--){
-					VBox drv1 = (VBox) pane.getChildren().get(dri);
-					VBox drv2 = (VBox) pane.getChildren().get(dri-1);
-					VBox drv3 = (VBox) pane.getChildren().get(dri-2);
-					VBox drv4 = (VBox) pane.getChildren().get(dri-3);
-					for(int drj=5; drj>2; drj--){
-						Circle drc1 = (Circle) drv1.getChildren().get(drj);
-						Circle drc2 = (Circle) drv2.getChildren().get(drj-1);
-						Circle drc3 = (Circle) drv3.getChildren().get(drj-2);
-						Circle drc4 = (Circle) drv4.getChildren().get(drj-3);
-						if((drc1.getFill() == Color.YELLOW && drc2.getFill() == Color.YELLOW && drc3.getFill() == Color.YELLOW && drc4.getFill() == Color.YELLOW) || (drc1.getFill() == Color.RED && drc2.getFill() == Color.RED && drc3.getFill() == Color.RED && drc4.getFill() == Color.RED)){
-							//FLASHING WIN
-							flashDisks(drc1, drc2, drc3, drc4, color);
-							//Disable clicking
-							disableClick(pane);
-						}
-					}
-				}
+				checkWin(pane, color);
 			});
 
 			//Add circle to VBox then VBox to pane
@@ -155,6 +83,97 @@ public class ConnectFour extends Application {
 		}
 	}
 
+	public void verticalLogic(FlowPane pane, Color color){
+		for(int i=0; i<6; i++){
+			VBox v = (VBox) pane.getChildren().get(i);
+			for(int j=5; j>2; j--){
+				Circle c1 = (Circle) v.getChildren().get(j);
+				Circle c2 = (Circle) v.getChildren().get(j-1);
+				Circle c3 = (Circle) v.getChildren().get(j-2);
+				Circle c4 = (Circle) v.getChildren().get(j-3);
+				if((c1.getFill() == Color.YELLOW && c2.getFill() == Color.YELLOW && c3.getFill() == Color.YELLOW && c4.getFill() == Color.YELLOW) || (c1.getFill() == Color.RED && c2.getFill() == Color.RED && c3.getFill() == Color.RED && c4.getFill() == Color.RED)){
+					//FLASHING WIN
+					flashDisks(c1, c2, c3, c4, color);
+					//Disable clicking
+					disableClick(pane);
+				}
+			}
+		}
+	}
+
+	public void horizontalLogic(FlowPane pane, Color color){
+		for(int i=0; i<4; i++){
+			VBox v1 = (VBox) pane.getChildren().get(i);
+			VBox v2 = (VBox) pane.getChildren().get(i+1);
+			VBox v3 = (VBox) pane.getChildren().get(i+2);
+			VBox v4 = (VBox) pane.getChildren().get(i+3);
+			for(int j=5; j>=0; j--){
+				Circle c1 = (Circle) v1.getChildren().get(j);
+				Circle c2 = (Circle) v2.getChildren().get(j);
+				Circle c3 = (Circle) v3.getChildren().get(j);
+				Circle c4 = (Circle) v4.getChildren().get(j);
+				if((c1.getFill() == Color.YELLOW && c2.getFill() == Color.YELLOW && c3.getFill() == Color.YELLOW && c4.getFill() == Color.YELLOW) || (c1.getFill() == Color.RED && c2.getFill() == Color.RED && c3.getFill() == Color.RED && c4.getFill() == Color.RED)){
+					//FLASHING WIN
+					flashDisks(c1, c2, c3, c4, color);
+					//Disable clicking
+					disableClick(pane);
+				}
+			}
+		}
+	}
+
+	public void diagonalLeftToRight(FlowPane pane, Color color){
+		for(int i=0; i<4; i++){
+			VBox v1 = (VBox) pane.getChildren().get(i);
+			VBox v2 = (VBox) pane.getChildren().get(i+1);
+			VBox v3 = (VBox) pane.getChildren().get(i+2);
+			VBox v4 = (VBox) pane.getChildren().get(i+3);
+			for(int j=5; j>2; j--){
+				Circle c1 = (Circle) v1.getChildren().get(j);
+				Circle c2 = (Circle) v2.getChildren().get(j-1);
+				Circle c3 = (Circle) v3.getChildren().get(j-2);
+				Circle c4 = (Circle) v4.getChildren().get(j-3);
+				if((c1.getFill() == Color.YELLOW && c2.getFill() == Color.YELLOW && c3.getFill() == Color.YELLOW && c4.getFill() == Color.YELLOW) || (c1.getFill() == Color.RED && c2.getFill() == Color.RED && c3.getFill() == Color.RED && c4.getFill() == Color.RED)){
+					//FLASHING WIN
+					flashDisks(c1, c2, c3, c4, color);
+					//Disable clicking
+					disableClick(pane);
+				}
+			}
+		}
+	}
+
+	public void diagonalRightToLeft(FlowPane pane, Color color){
+		for(int i=6; i>2; i--){
+			VBox v1 = (VBox) pane.getChildren().get(i);
+			VBox v2 = (VBox) pane.getChildren().get(i-1);
+			VBox v3 = (VBox) pane.getChildren().get(i-2);
+			VBox v4 = (VBox) pane.getChildren().get(i-3);
+			for(int j=5; j>2; j--){
+				Circle c1 = (Circle) v1.getChildren().get(j);
+				Circle c2 = (Circle) v2.getChildren().get(j-1);
+				Circle c3 = (Circle) v3.getChildren().get(j-2);
+				Circle c4 = (Circle) v4.getChildren().get(j-3);
+				if((c1.getFill() == Color.YELLOW && c2.getFill() == Color.YELLOW && c3.getFill() == Color.YELLOW && c4.getFill() == Color.YELLOW) || (c1.getFill() == Color.RED && c2.getFill() == Color.RED && c3.getFill() == Color.RED && c4.getFill() == Color.RED)){
+					//FLASHING WIN
+					flashDisks(c1, c2, c3, c4, color);
+					//Disable clicking
+					disableClick(pane);
+				}
+			}
+		}
+	}
+
+	public void checkWin(FlowPane pane, Color color){
+		//VERTICAL
+		verticalLogic(pane, color);
+		//HORIZONTAL
+		horizontalLogic(pane, color);
+		//DIAGONAL: LEFT TO RIGHT
+		diagonalLeftToRight(pane, color);
+		//DIAGONAL: RIGHT TO LEFT
+		diagonalRightToLeft(pane, color);
+	}
 	public static void main(String[] args) {
 		System.out.println("Running GUI...");
 		launch(args);
